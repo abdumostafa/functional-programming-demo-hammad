f0f829946a2f3c43b0aab1fd639559ec3241aed1
AbdElmneim Hussein <abdumostafa@outlook.com>
2021-04-16 16:10:52 +0300

    create repo

---
+++ ConsoleApp19/Demo.cs
@@ -0,0 +1,90 @@
+﻿//INSTANT C# NOTE: Formerly VB project-level imports:
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+using static System.Tuple;
+using static System.ValueTuple;
+
+namespace ConsoleApp19
+{
+	public class Demo
+    {
+    private List<Order> OrdersforProcessing = new List<Order>();
+
+    public void Main()
+        {
+            var OrderswithDiscounts = OrdersforProcessing.Select((x) => GetOrderwithDiscount(x, GetDiscountRules()));
+        }
+           
+       public Order GetOrderwithDiscount(Order R, List<(Func<Order, bool> QualifyingCondition, Func<Order, decimal> GetDiscount)> Rules)
+        {
+        	var discount = Rules.Where((a) => a.QualifyingCondition(R)).Select((b) => b.GetDiscount(R)).OrderBy((x) => x).Take(3).Average();
+            var neworder = new Order(R);
+            neworder.Discount = discount;
+        	return neworder;
+        }
+
+       public List<(Func<Order, bool> QualifyingCondition, Func<Order, decimal> GetDiscount)>  GetDiscountRules()
+        {
+            List<(Func<Order, bool> QualifyingCondition, Func<Order, decimal> GetDiscount)> DiscountRules 
+              = new List<(Func<Order, bool> QualifyingCondition, Func<Order, decimal> GetDiscount)>
+            { ((isAQualified, A)),
+              ((isBQualified, B)),
+              ((isCQualified, C)) };
+
+            return DiscountRules;
+        }
+      
+        
+        
+        
+        public bool isAQualified(Order r)
+        {
+            return true;
+        }
+        public decimal A(Order r)
+        {
+
+            return 1M;
+        }
+        public bool isBQualified(Order r)
+        {
+            return true;
+        }
+        public decimal B(Order r)
+        {
+
+            return 1M;
+        }
+        public bool isCQualified(Order r)
+        {
+            return true;
+        }
+        public decimal C(Order r)
+        {
+
+            return 1M;
+        }
+
+
+    }
+
+
+	public class Order
+	{
+		public Order(Order orderdata)
+		{
+
+		}
+		public decimal Discount;
+
+	}
+
+
+}
---
+++ ConsoleApp19/Properties/AssemblyInfo.cs
@@ -0,0 +1,50 @@
+﻿//INSTANT C# NOTE: Formerly VB project-level imports:
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("ConsoleApp19")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp19")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4770d3b-dda3-482b-8f19-5a3f7800108d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")>
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
